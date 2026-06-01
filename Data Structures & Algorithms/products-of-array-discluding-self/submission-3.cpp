449d299381160c8e92bc35cd6b78a7562340b424
Gaurav Jain <161191559+LesssggooGauravv@users.noreply.github.com>
2026-06-01 15:10:29 +0530

    Add: products-of-array-discluding-self - submission-3

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-3.cpp
@@ -0,0 +1,69 @@
+// class Solution {
+// public:
+//     vector<int> productExceptSelf(vector<int>& nums) {
+//         vector<int> prod;
+//         for(int i=0;i<nums.size();i++){
+//             int mul=1;
+//             for(int j=0;j<nums.size();j++){
+//                 if(j==i) continue;
+//                 mul*=nums[j];
+//             }
+//             prod.push_back(mul);
+//         }
+//         return prod;
+//     }
+// };
+
+// class Solution {
+// public:
+//     vector<int> productExceptSelf(vector<int>& nums) {
+//         int mul=1;
+//         int zero=0;
+//         for(int i=0;i<nums.size();i++){
+//             if(nums[i]!=0) mul*=nums[i];
+//             else zero++;
+//         }
+//         if(zero>1) return vector<int>(nums.size(),0);
+//         vector<int> prod(nums.size());
+//         for(int i=0;i<nums.size();i++){
+//             if(zero>0) prod[i]=(nums[i]==0) ? mul:0;
+//             else prod[i] = mul/nums[i];
+//         }
+//         return prod;
+//     }
+// };
+
+// class Solution {
+// public:
+//     vector<int> productExceptSelf(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> res(n);
+//         vector<int> pref(n);
+//         vector<int> suff(n);
+//         pref[0] = 1;
+//         suff[n-1] = 1;
+//         for(int i=1;i<n;i++) 
+//             pref[i] = nums[i-1]*pref[i-1];
+//         for(int i=n-2;i>=0;i--) 
+//             suff[i] = nums[i+1]*suff[i+1];
+//         for(int i=0;i<n;i++)
+//             res[i] = pref[i]*suff[i];
+//         return res;
+//     }
+// };
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> res(n,1);
+        for(int i=1;i<n;i++)
+            res[i] = res[i-1] * nums[i-1];
+        int post = 1;
+        for(int i=n-1;i>=0;i--){
+            res[i]*=post;
+            post*=nums[i];
+        }
+        return res;
+    }
+};
