ac4c71f12af0d401724b5832e47cffbd07422785
Gaurav Jain <161191559+LesssggooGauravv@users.noreply.github.com>
2026-06-04 13:21:03 +0530

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.cpp
@@ -0,0 +1,35 @@
+// class Solution {
+// public:
+//     vector<int> dailyTemperatures(vector<int>& temp) {
+//         vector<int> ans(temp.size(),0);
+//         for(int i=0;i<temp.size();i++){
+//             for(int j=i+1;j<temp.size();j++){
+//                 if(temp[i]<temp[j]){
+//                     ans[i]=j-i;
+//                     break;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temp) {
+        int n = temp.size();
+        vector<int> res(n,0);
+        stack<pair<int,int>>stack; //{temp[i],i}
+        for(int i=0;i<n;i++){
+            int t = temp[i];
+            while(!stack.empty() && t>stack.top().first){
+                auto pair=stack.top();
+                stack.pop();
+                res[pair.second] = i-pair.second;
+            }
+            stack.push({t,i});
+        }
+        return res;
+    }
+};
