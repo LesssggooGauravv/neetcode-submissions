db11c5f0f5668ee10dc7729cd5d4096171e0accf
Gaurav Jain <161191559+LesssggooGauravv@users.noreply.github.com>
2026-06-07 14:39:56 +0530

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.cpp
@@ -0,0 +1,38 @@
+// class Solution {
+// public:
+//     int minEatingSpeed(vector<int>& piles, int h) {
+//         int speed = 1;
+//         while(true){
+//             long long time=0;
+//             for(int pile:piles){
+//                 time += (pile+speed-1)/speed;
+//             }
+//             if(time<=h) return speed;
+//             speed++;
+//         }
+//     }
+// };
+
+class Solution {
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int l=1;
+        int r=*max_element(piles.begin(),piles.end());
+        int res=r;
+        while(l<=r){
+            int k = l+(r-l)/2;
+            long long time=0;
+            for(int p:piles){
+                time+=ceil(static_cast<double>(p)/k);
+            }
+            if(time<=h){
+                res=k;
+                r=k-1;
+            }
+            else{
+                l=k+1;
+            }
+        }
+        return res;
+    }
+};
