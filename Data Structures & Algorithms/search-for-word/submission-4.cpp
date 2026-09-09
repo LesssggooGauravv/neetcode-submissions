757d2f167f43f237d3592f4f1b901fede94881f4
Gaurav Jain <161191559+LesssggooGauravv@users.noreply.github.com>
2026-09-09 16:15:32 +0530

    Add: search-for-word - submission-4

---
+++ Data Structures & Algorithms/search-for-word/submission-4.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    set<pair<int,int>>path;
+    int n,m;
+    bool exist(vector<vector<char>>& board, string word) {
+        n=board.size(),m=board[0].size();
+        for(int r=0;r<n;r++){
+            for(int c=0;c<m;c++){
+                if(dfs(0,r,c,board,word)) return true;
+            }
+        }
+        return false;
+    }
+    bool dfs(int i,int r,int c,vector<vector<char>>&board,string word){
+        if(i==word.size()) return true;
+        if(r<0 || c<0 || r>=n || c>=m || word[i]!=board[r][c] || path.count({r,c})) return false;
+        path.insert({r,c});
+        bool res = dfs(i+1,r+1,c,board,word) || 
+            dfs(i+1,r-1,c,board,word) || 
+            dfs(i+1,r,c+1,board,word) || 
+            dfs(i+1,r,c-1,board,word);
+        path.erase({r,c});
+        return res;
+    }
+};
